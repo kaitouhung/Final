32eccaf95c16142fc96aa48f007faa5bb6a3df85
Itachi <kaitouhung@gmail.com>
2021-12-17 15:38:25 +0700

    First Step

---
+++ crawl/src/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+
+const crawlPost = async () => {
+  try {
+    const categories = ['business', 'health', 'sports', 'science'];
+
+    const postFollowCategory = await Promise.all(
+      categories.map(async (category) => {
+        const urlNews = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=20&apiKey=${process.env.NEWS_KEY}`;
+
+        const newsResponse = await axios.get(urlNews);
+
+        const postListCategory = [];
+        for (let i = 0; i < newsResponse.data.articles.length; i++) {
+          const post = newsResponse.data.articles[i];
+          if (post.author !== null && post.content !== null) {
+            postListCategory.push({
+              title: post.title,
+              description: post.description,
+              content: post.content,
+              author: post.author,
+              image: post.urlToImage,
+              category: category,
+              createAt: post.publishedAt,
+            });
+          }
+        }
+
+        return postListCategory;
+      })
+    );
+
+    const postList = [].concat.apply([], postFollowCategory);
+    console.log(postList);
+  } catch (err) {
+    return res.status(500).json({
+      status: 'Error',
+      message: err.message,
+    });
+  }
+};
+
+crawlPost();
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server Crawl is running on ${PORT}`);
+});
