8888527b375cfce67a1a12401adbb39fa5a48ea0
leanhkhoa1803 <46062409+leanhkhoa1803@users.noreply.github.com>
2022-01-09 16:45:11 +0700

    up client-admin

--- src/App.js
+++ src/App.js
@@ -1,71 +1,75 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Switch,  Route, Redirect} from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Common from './pages/Common'
-import Customer from './pages/Customer'
-import AdminCommon from './pages/admin/AdminCommon'
-import UserList from './pages/admin/UserList';
-import Order from './pages/admin/Order';
-import {authentication} from './pages/authentication'
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Common from "./pages/Common";
+import Customer from "./pages/Customer";
+import AdminCommon from "./pages/admin/AdminCommon";
+import UserList from "./pages/admin/UserList";
+import Order from "./pages/admin/Order";
+import { authentication } from "./pages/authentication";
+import AddPost from "./pages/admin/AddPost";
 
 function App() {
   return (
     <BrowserRouter>
-      <RouteWrapper exact path="/" component={Home} layout={Common}/>
-      <RouteWrapper path="/customer" component={Customer} layout={Common}/>
+      <RouteWrapper exact path="/" component={Home} layout={Common} />
+      <RouteWrapper path="/customer" component={Customer} layout={Common} />
 
-      <PrivateRoute path="/admin" component={UserList} layout={AdminCommon}/>
-      <RouteWrapper path="/order" component={Order} layout={AdminCommon}/>
+      <RouteWrapper path="/admin" component={UserList} layout={AdminCommon} />
+      <RouteWrapper path="/order" component={Order} layout={AdminCommon} />
+      <RouteWrapper path="/add-post" component={AddPost} layout={AdminCommon} />
 
-      <Route path="/login" component={Login}/>
-      <Route path="/register" component={Register}/>
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </BrowserRouter>
   );
 }
 
-function RouteWrapper ({component: Component,  layout: Layout,  ...rest}) {
+function RouteWrapper({ component: Component, layout: Layout, ...rest }) {
   return (
     <Route
       {...rest}
-      render = {(props) => (
+      render={(props) => (
         <Layout {...props}>
-          <Component {...props}/>
+          <Component {...props} />
         </Layout>
       )}
     />
-  )
+  );
 }
 
-function PrivateRoute({component: Component, layout: Layout, ...rest}) {
+function PrivateRoute({ component: Component, layout: Layout, ...rest }) {
   return (
     <Route
       {...rest}
-      render={props => (
-        authentication.isAuthentication() ?
-        (<Layout {...props}>
-          <Component {...props}/>
-        </Layout>)
-        :
-        (<Redirect to="/login" />)
-        )}
+      render={(props) =>
+        authentication.isAuthentication() ? (
+          <Layout {...props}>
+            <Component {...props} />
+          </Layout>
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
     />
-  )
+  );
 }
 
-function PrivateRoute2({component: Component, ...rest}) {
+function PrivateRoute2({ component: Component, ...rest }) {
   return (
     <Route
       {...rest}
-      render={props => (
-        authentication.isAuthentication() ?
-        (<Component {...props}/>)
-        :
-        (<Redirect to="/login" />)
-        )}
+      render={(props) =>
+        authentication.isAuthentication() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
     />
-  )
+  );
 }
 
 export default App;
